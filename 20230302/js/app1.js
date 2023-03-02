d1ac6f70c203cc159f15db176aae1865b9847514
freelancerfume <freelancerfume@gmail.com>
2023-03-02 20:02:34 +0300

    20230302 JS Examples

---
+++ 20230302/js/app1.js
@@ -0,0 +1,14 @@
+var x1 = "merhaba";
+let x2 = 52;
+var x3 = false;
+const x4 = [];
+var x5 = function myFunction(){};
+var x6 = {};
+
+document.getElementById("type").innerHTML=
+            typeof x1 + "</br>" + 
+            typeof x2 + "</br>" + 
+            typeof x3 + "</br>" + 
+            typeof x4 + "</br>" + 
+            typeof x5 + "</br>" + 
+            typeof x6;
